08715655c233a909f111eb083134c5e73ae213fb
cafefish <yu@mpdl.mpg.de>
2014-08-12 18:01:32 +0200

    css update 1

--- src/main/webapp/resources/js/custom.js
+++ src/main/webapp/resources/js/custom.js
@@ -4,6 +4,5 @@
 $(function(e) {
 	var obj = $('#top').find('.ui-layout-unit-content');
 	obj.css({"background":"#51BEDB"});
-	
-
+//	$('.ui-fileupload-buttonbar').css({"background":"#51BEDB"});
 });
