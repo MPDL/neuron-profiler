d0b9c0d2aedde9c73b326c727bd000d9e4a7585c
Bastien Saquet <saquet@mpdl.mpg.de>
2014-08-20 15:18:58 +0200

    modify git ignore and remove target

--- target/neuron-profiler-0.0.1-SNAPSHOT/resources/js/custom.js
+++
@@ -1,8 +0,0 @@
-/**
- * 
- */
-$(function(e) {
-	var obj = $('#top').find('.ui-layout-unit-content');
-	obj.css({"background":"#51BEDB"});
-//	$('.ui-fileupload-buttonbar').css({"background":"#51BEDB"});
-});
